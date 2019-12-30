11fe730f5a1dcc3d7bff7117f03a9eb2d46b88ef
Florian Romijn <1840215romijn@zuyd.nl>
2019-12-30 20:56:21 +0100

    DeelnemersController aangemaakt en acties eraan toegevoegd. Weet alleen niet hoe ik aan het evenementId ga komen voor de insert in de database

--- Event manager v2/Controllers/DeelnemersController.cs
+++ Event manager v2/Controllers/DeelnemersController.cs
@@ -10,110 +10,113 @@ using Event_manager_v2.Models;
 
 namespace Event_manager_v2.Controllers
 {
-    public class EvenementsController : Controller
+    public class DeelnemersController : Controller
     {
         private DataModelContext db = new DataModelContext();
 
-        // GET: Evenements
+        // GET: Deelnemers
         public ActionResult Index()
         {
-            return View(db.Evenements.ToList());
+            var deelnemers = db.Deelnemers.Include(d => d.Evenement1);
+            return View(deelnemers.ToList());
         }
 
-        // GET: Evenements/Details/5
+        // GET: Deelnemers/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Evenement evenement = db.Evenements.Find(id);
-            if (evenement == null)
+            Deelnemer deelnemer = db.Deelnemers.Find(id);
+            if (deelnemer == null)
             {
                 return HttpNotFound();
             }
-            return View(evenement);
+            return View(deelnemer);
         }
 
-        // GET: Evenements/Create
-        public ActionResult Create()
+        // GET: Deelnemers/SchrijfIn
+        public ActionResult SchrijfIn()
         {
             return View();
         }
 
-        // POST: Evenements/Create
+        // POST: Deelnemers/SchrijfIn
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "evenement_id,naam,beschrijving,begindatum,einddatum")] Evenement evenement)
+        public ActionResult SchrijfIn([Bind(Include = "deelnemer_id,voornaam,achternaam,email,evenement,goedgekeurd")] Deelnemer deelnemer)
         {
             if (ModelState.IsValid)
             {
-                db.Evenements.Add(evenement);
+                db.Deelnemers.Add(deelnemer);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(evenement);
+            
+            return View(deelnemer);
         }
 
-        // GET: Evenements/Edit/5
+        // GET: Deelnemers/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Evenement evenement = db.Evenements.Find(id);
-            if (evenement == null)
+            Deelnemer deelnemer = db.Deelnemers.Find(id);
+            if (deelnemer == null)
             {
                 return HttpNotFound();
             }
-            return View(evenement);
+            ViewBag.evenement = new SelectList(db.Evenements, "evenement_id", "naam", deelnemer.evenement);
+            return View(deelnemer);
         }
 
-        // POST: Evenements/Edit/5
+        // POST: Deelnemers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Edit([Bind(Include = "evenement_id,naam,beschrijving,begindatum,einddatum")] Evenement evenement)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "deelnemer_id,voornaam,achternaam,email,evenement,goedgekeurd")] Deelnemer deelnemer)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(evenement).State = EntityState.Modified;
+                db.Entry(deelnemer).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(evenement);
+            ViewBag.evenement = new SelectList(db.Evenements, "evenement_id", "naam", deelnemer.evenement);
+            return View(deelnemer);
         }
 
-        // GET: Evenements/Delete/5
+        // GET: Deelnemers/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Evenement evenement = db.Evenements.Find(id);
-            if (evenement == null)
+            Deelnemer deelnemer = db.Deelnemers.Find(id);
+            if (deelnemer == null)
             {
                 return HttpNotFound();
             }
-            return View(evenement);
+            return View(deelnemer);
         }
 
-        // POST: Evenements/Delete/5
+        // POST: Deelnemers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Evenement evenement = db.Evenements.Find(id);
-            db.Evenements.Remove(evenement);
+            Deelnemer deelnemer = db.Deelnemers.Find(id);
+            db.Deelnemers.Remove(deelnemer);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-
 
         protected override void Dispose(bool disposing)
         {
